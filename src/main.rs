2eb478a98920594cc539129d93b588c64840001d
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2024-04-07 13:15:54 +0100

    Finish exercise 4

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use axum::{
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
-    Router,
+    Json, Router,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -36,6 +36,27 @@ struct Reindeer {
     strength: u64,
 }
 
+#[derive(Deserialize, Debug)]
+struct ContestReindeer {
+    name: String,
+    strength: u64,
+    speed: f64,
+    height: u64,
+    antler_width: u64,
+    snow_magic_power: u64,
+    favorite_food: String,
+    #[serde(rename = "cAnD13s_3ATeN-yesT3rdAy")]
+    candies_eaten_yesterday: u64,
+}
+
+#[derive(Debug, Serialize)]
+struct ContestResult {
+    fastest: String,
+    tallest: String,
+    magician: String,
+    consumer: String,
+}
+
 async fn calculate_strength(
     extract::Json(reindeers): extract::Json<Vec<Reindeer>>,
 ) -> impl IntoResponse {
@@ -45,13 +66,64 @@ async fn calculate_strength(
         .to_string()
 }
 
+async fn calculate_contest(
+    extract::Json(reindeers): extract::Json<Vec<ContestReindeer>>,
+) -> Result<Json<ContestResult>, StatusCode> {
+    if reindeers.len() < 1 {
+        return Err(StatusCode::BAD_REQUEST);
+    }
+
+    let mut fastest = &reindeers[0];
+    let mut tallest = &reindeers[0];
+    let mut magician = &reindeers[0];
+    let mut consumer = &reindeers[0];
+
+    for reindeer in reindeers.iter() {
+        if reindeer.speed > fastest.speed {
+            fastest = &reindeer;
+        }
+
+        if reindeer.height > tallest.height {
+            tallest = &reindeer;
+        }
+
+        if reindeer.snow_magic_power > magician.snow_magic_power {
+            magician = &reindeer;
+        }
+
+        if reindeer.candies_eaten_yesterday > consumer.candies_eaten_yesterday {
+            consumer = &reindeer;
+        }
+    }
+
+    Ok(Json(ContestResult {
+        fastest: format!(
+            "Speeding past the finish line with a strength of {} is {}",
+            fastest.strength, fastest.name
+        ),
+        tallest: format!(
+            "{} is standing tall with his {} cm wide antlers",
+            tallest.name, tallest.antler_width
+        ),
+        magician: format!(
+            "{} could blast you away with a snow magic power of {}",
+            magician.name, magician.snow_magic_power
+        ),
+        consumer: format!(
+            "{} ate lots of candies, but also some {}",
+            consumer.name, consumer.favorite_food
+        ),
+    }))
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/error", get(internal_server_error))
         .route("/1/*nums", get(recalibrate_multiple_packet_ids))
-        .route("/4/*nums", post(calculate_strength));
+        .route("/4/strength", post(calculate_strength))
+        .route("/4/contest", post(calculate_contest));
 
     Ok(router.into())
 }
