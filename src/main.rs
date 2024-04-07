eedfb401350ffe848478ba038aa7d007203d8554
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2024-04-07 12:46:29 +0100

    Finished exercise 4 task 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use axum::{
+    extract::{self, Path},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Router,
+};
+use serde::Deserialize;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -23,12 +30,28 @@ async fn recalibrate_multiple_packet_ids(Path(nums): Path<String>) -> Result<Str
     Ok(xor.pow(3).to_string())
 }
 
+#[derive(Deserialize)]
+struct Reindeer {
+    name: String,
+    strength: u64,
+}
+
+async fn calculate_strength(
+    extract::Json(reindeers): extract::Json<Vec<Reindeer>>,
+) -> impl IntoResponse {
+    reindeers
+        .iter()
+        .fold(0u64, |sum, reindeer| sum + reindeer.strength)
+        .to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/error", get(internal_server_error))
-        .route("/1/*nums", get(recalibrate_multiple_packet_ids));
+        .route("/1/*nums", get(recalibrate_multiple_packet_ids))
+        .route("/4/*nums", post(calculate_strength));
 
     Ok(router.into())
 }
